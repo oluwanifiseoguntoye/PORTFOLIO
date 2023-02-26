cd20111859422cc38fdc23dba614fb9b713e517a
Oluwanifise Oguntoye <117686447+oluwanifiseoguntoye@users.noreply.github.com>
2023-02-26 18:33:29 -0500

    hover effects sorted, drop down menu next

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const navLinks = document.querySelectorAll('.nav-link');
 
-  navLinks.forEach(navLink => {
+navLinks.forEach(navLink => {
     navLink.addEventListener('click', function() {
       // Add the 'active' class to the clicked nav item
       navLinks.forEach(link => link.classList.remove('active'));
