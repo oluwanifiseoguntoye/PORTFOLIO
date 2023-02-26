93be1fceb6f037cd6a22bae8d250d6e93b1a32e3
Oluwanifise Oguntoye <117686447+oluwanifiseoguntoye@users.noreply.github.com>
2023-02-26 03:58:55 -0500

    Fixed collapse

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ const navLinks = document.querySelectorAll('.nav-link');
       this.classList.add('active');
     });
   });
+
+let button = document.querySelector('.navbar-toggler');
+let menu = document.querySelector('.collapse');
+  
+button.addEventListener('click', function() {
+  menu.classList.toggle('show');
+});
+  
