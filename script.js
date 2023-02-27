2d726e54445792e7848491d23ef47b33296cec85
Oluwanifise Oguntoye <117686447+oluwanifiseoguntoye@users.noreply.github.com>
2023-02-27 16:10:34 -0500

    fixed drop down

--- script.js
+++ script.js
@@ -2,16 +2,29 @@ const navLinks = document.querySelectorAll('.nav-link');
 
 navLinks.forEach(navLink => {
     navLink.addEventListener('click', function() {
-      // Add the 'active' class to the clicked nav item
-      navLinks.forEach(link => link.classList.remove('active'));
-      this.classList.add('active');
+        // Add the 'active' class to the clicked nav item
+        navLinks.forEach(link => link.classList.remove('active'));
+        this.classList.add('active');
     });
-  });
-
-let button = document.querySelector('.navbar-toggler');
-let menu = document.querySelector('.collapse');
-  
-button.addEventListener('click', function() {
-  menu.classList.toggle('show');
 });
-  
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+function show(){
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
+
+function close(){
+  mainMenu.style.top = '-100%';
+  mainMenu.addEventListener('transitionend', function() {
+      mainMenu.style.display = 'none';
+      mainMenu.removeEventListener('transitionend', arguments.callee, false);
+  }, {once: true});
+}
+
